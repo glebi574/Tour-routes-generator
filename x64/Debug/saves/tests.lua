e276fd089272dd9b60bf1713c5ac5b6a16eaa931
glebi574 <glebi574@gmail.com>
2023-03-25 15:08:25 +0200

    Update

---
+++ x64/Debug/saves/tests.lua
@@ -0,0 +1,68 @@
+first_point = 0
+last_point = 4
+amount = 20
+cycles_amount = 10
+time_ratio = 1
+price_ratio = 1
+score_ratio = 1
+
+map = {
+	{
+		time = 0,
+		price = 0,
+		score = 0,
+		name = "Музей",
+		x = 371,
+		y = 287,
+		connections = { 0, 12.87, 19.42, 0.63, 10.89, }
+	},
+	{
+		time = 5.15,
+		price = 0,
+		score = 4.4,
+		name = "Музей 2",
+		x = 384,
+		y = 439,
+		connections = { 12.87, 0, 10.01, 20.15, 15.42, }
+	},
+	{
+		time = 8.21,
+		price = 0,
+		score = 10.16,
+		name = "Река",
+		x = 535,
+		y = 450,
+		connections = { 19.42, 10.01, 0, 0, 11.09, }
+	},
+	{
+		time = 7.52,
+		price = 0,
+		score = 7.94,
+		name = "Дорога",
+		x = 651,
+		y = 316,
+		connections = { 0.63, 20.15, 0, 0, 9.15, }
+	},
+	{
+		time = 4.27,
+		price = 0,
+		score = 5.44,
+		name = "Поле",
+		x = 539,
+		y = 184,
+		connections = { 10.89, 15.42, 11.09, 9.15, 0, }
+	},
+}
+
+connections = {
+	{ id1 = 3, id2 = 2, time = 6.18 },
+	{ id1 = 4, id2 = 2, time = 11.09 },
+	{ id1 = 0, id2 = 2, time = 19.42 },
+	{ id1 = 1, id2 = 2, time = 10.01 },
+	{ id1 = 4, id2 = 1, time = 15.42 },
+	{ id1 = 3, id2 = 1, time = 20.15 },
+	{ id1 = 3, id2 = 0, time = 0.63 },
+	{ id1 = 4, id2 = 0, time = 10.89 },
+	{ id1 = 4, id2 = 3, time = 9.15 },
+	{ id1 = 1, id2 = 0, time = 12.87 },
+}
